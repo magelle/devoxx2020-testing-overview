b72bc2995ec55310e7b1b8902c453804ac1388ec
Maxime GELLE <maxime.gelle@gmail.com>
2020-02-16 23:38:11 +0100

    Add ArchUnit

---
+++ workshop/discount/src/test/java/architecture/HexagonalArchitectureTest.java
@@ -0,0 +1,24 @@
+package architecture;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packages = "devoxxfr2020", importOptions = {ImportOption.DoNotIncludeTests.class})
+public class HexagonalArchitectureTest {
+
+    @ArchTest
+    public static ArchRule enforceHexagonalArchitechture = classes()
+            .that().resideInAPackage("devoxxfr2020.cashregister.domain..")
+            .should()
+            .onlyAccessClassesThat()
+            .resideInAnyPackage(
+                    "devoxxfr2020.cashregister.domain..",
+                    "java.util..",
+                    "java.lang.."
+            );
+
+}
