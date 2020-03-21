5c156c77f883abe986d0c285db48e20ad4728cad
Yannick Grenzinger <yannick.grenzinger@gmail.com>
2020-03-21 17:05:31 +0100

    less verbose LocalizedAppleDiscountTests. Question about parameterized tests ?

--- workshop/discount/src/test/java/devoxxfr2020/cashregister/domain/discount/LocalizedAppleDiscountTest.java
+++ workshop/discount/src/test/java/devoxxfr2020/cashregister/domain/discount/LocalizedAppleDiscountTest.java
@@ -18,25 +18,32 @@ class LocalizedAppleDiscountTest {
         assertThat(applicable).isTrue();
     }
 
-    @Test
+    @Test // parameterized tests ?
     public void should_be_applicable_when_more_than_4_apples_in_the_same_language() {
-        assertThat(localizedAppleDiscount.isApplicable(List.of(new BasketItem("Apples", 4)))).isTrue();
-        assertThat(localizedAppleDiscount.isApplicable(List.of(new BasketItem("Mele", 4)))).isTrue();
-        assertThat(localizedAppleDiscount.isApplicable(List.of(new BasketItem("Pommes", 4)))).isTrue();
+        discountShouldBeApplicableFor(new BasketItem("Apples", 4));
+        discountShouldBeApplicableFor(new BasketItem("Mele", 4));
+        discountShouldBeApplicableFor(new BasketItem("Pommes", 4));
     }
 
     @Test
-    public void should_be_applicable_when_less_than_4_apples_in_different_languages() {
+    public void should_not_be_applicable_when_less_than_4_apples_in_different_languages() {
         List<BasketItem> fruits = List.of(new BasketItem("Apples", 1), new BasketItem("Mele", 1), new BasketItem("Pommes", 1));
-        boolean applicable = localizedAppleDiscount.isApplicable(fruits);
-        assertThat(applicable).isFalse();
+        assertThat(localizedAppleDiscount.isApplicable(fruits)).isFalse();
     }
 
-    @Test
-    public void should_be_applicable_when_less_than_4_apples_in_the_same_language() {
-        assertThat(localizedAppleDiscount.isApplicable(List.of(new BasketItem("Apples", 3)))).isFalse();
-        assertThat(localizedAppleDiscount.isApplicable(List.of(new BasketItem("Mele", 3)))).isFalse();
-        assertThat(localizedAppleDiscount.isApplicable(List.of(new BasketItem("Pommes", 3)))).isFalse();
+    @Test // parameterized tests ?
+    public void should_not_be_applicable_when_less_than_4_apples_in_the_same_language() {
+        discountShouldNotBeApplicableFor(new BasketItem("Apples", 3));
+        discountShouldNotBeApplicableFor(new BasketItem("Mele", 3));
+        discountShouldNotBeApplicableFor(new BasketItem("Pommes", 3));
+    }
+
+    private void discountShouldNotBeApplicableFor(BasketItem pommes) {
+        assertThat(localizedAppleDiscount.isApplicable(List.of(pommes))).isFalse();
+    }
+
+    private void discountShouldBeApplicableFor(BasketItem apples) {
+        assertThat(localizedAppleDiscount.isApplicable(List.of(apples))).isTrue();
     }
 
 }
