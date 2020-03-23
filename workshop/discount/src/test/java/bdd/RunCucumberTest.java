888324f0536cb98ff88876e2337a527642995396
Yannick Grenzinger <yannick.grenzinger@gmail.com>
2020-03-23 06:25:59 +0100

    Removing junit-vintage and using Junit 5 for Cucumber

--- workshop/discount/src/test/java/bdd/RunCucumberTest.java
+++ workshop/discount/src/test/java/bdd/RunCucumberTest.java
@@ -1,10 +1,7 @@
 package bdd;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Cucumber;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"json:target/cucumber.json"}, strict = true)
+@Cucumber
 public class RunCucumberTest {
 }
